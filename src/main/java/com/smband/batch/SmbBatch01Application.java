481aa316181e57d1ab43ef1aaef7449d275b21c3
ytkim <ytkim@smband.co.kr>
2023-03-27 16:21:16 +0900

    초기 소스

---
+++ src/main/java/com/smband/batch/SmbBatch01Application.java
@@ -0,0 +1,13 @@
+package com.smband.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmbBatch01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmbBatch01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/smband/batch/SmbBatch01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smband.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmbBatch01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
